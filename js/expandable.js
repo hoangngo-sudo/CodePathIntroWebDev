ec9987c1cb28ba35d1a9daa320fc693eec4386d1
Minh Hoang Ngo <mngo12332@gmail.com>
2025-06-20 23:46:52 -0500

    update files

--- js/expandable.js
+++ js/expandable.js
@@ -2,6 +2,7 @@ function expandable() {
   const expandTriggers = document.querySelectorAll(".expand-trigger");
   expandTriggers.forEach((trigger) => {
     trigger.addEventListener("click", function () {
+      trigger.id = 'expand-trigger';
       // Get the parent element
       const parentItem = this.parentElement;
       parentItem.classList.toggle("active");
